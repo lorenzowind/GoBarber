b944d4f2958ca0410c528d0d9d297dd525291a5e
lorenzowind <lorenzomart01@gmail.com>
2020-07-01 18:41:05 -0400

    feat: routes fields valitation

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,12 +1,11 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import { errors } from 'celebrate';
 import 'express-async-errors';
 
 import uploadConfig from '@config/upload';
-
 import routes from './routes';
-
 import AppError from '../../errors/AppError';
 
 import '../typeorm/database';
@@ -17,6 +16,8 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
