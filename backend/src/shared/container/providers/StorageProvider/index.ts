070ecb702c75a2cbc9488b5571479cbe2b322e7f
lorenzowind <lorenzomart01@gmail.com>
2020-07-02 22:19:16 -0400

    feat: s3 files upload service

--- backend/src/shared/container/providers/StorageProvider/index.ts
+++ backend/src/shared/container/providers/StorageProvider/index.ts
@@ -1,14 +1,13 @@
 import { container } from 'tsyringe';
 
 import DiskStorageProvider from './implementations/DiskStorageProvider';
+import S3StorageProvider from './implementations/S3StorageProvider';
 
 import IStorageProvider from './models/IStorageProvider';
 
 const providers = {
   disk: DiskStorageProvider,
+  s3: S3StorageProvider,
 };
 
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  providers.disk,
-);
+container.registerSingleton<IStorageProvider>('StorageProvider', providers.s3);
