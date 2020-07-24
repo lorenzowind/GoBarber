2ad2b54c7c7081534960464ef2e1d01fa1a4e33e
lorenzowind <lorenzomart01@gmail.com>
2020-07-24 19:31:50 -0400

    feat: interface signin component implementation

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,33 +1,79 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
 
-      <Title>Faça seu logon</Title>
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
 
-      <Input name="password" icon="lock" placeholder="Senha" />
+            <Input name="password" icon="lock" placeholder="Senha" />
 
-      <Button
+            <Button
+              onPress={() => {
+                // eslint-disable-next-line no-console
+                console.log('Working...');
+              }}
+            >
+              Entrar
+            </Button>
+
+            <ForgotPassword
+              onPress={() => {
+                console.log('Working...');
+              }}
+            >
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton
         onPress={() => {
-          // eslint-disable-next-line no-console
           console.log('Working...');
         }}
       >
-        Entrar
-      </Button>
-    </Container>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
