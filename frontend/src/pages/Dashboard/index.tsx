e0260394389ac761d0bbd16536794084c123453d
lorenzowind <lorenzomart01@gmail.com>
2020-08-10 23:57:16 -0400

    feat: interface profile page

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -9,6 +9,7 @@ import DayPicker, { DayModifiers } from 'react-day-picker';
 
 import 'react-day-picker/lib/style.css';
 
+import { Link } from 'react-router-dom';
 import { useAuth } from '../../hooks/auth';
 
 import {
@@ -159,7 +160,9 @@ const Dashboard: React.FC = () => {
             )}
             <div>
               <span>Bem-vindo,</span>
-              <strong>{user.name}</strong>
+              <Link to="profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
